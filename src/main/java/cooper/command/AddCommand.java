1385c1157f4eb7f985681797715dcdf321d5526f
ChrisLangton <christwlangton@gmail.com>
2021-10-03 16:54:02 +0800

    update AboutUs image

--- src/main/java/cooper/command/AddCommand.java
+++ src/main/java/cooper/command/AddCommand.java
@@ -2,12 +2,12 @@ package cooper.command;
 
 public class AddCommand extends Command {
 
-    public AddCommand() {
+    public AddCommand(String cashFlow) {
         super();
     }
 
     public void execute() {
-        System.out.println("I want to execute this [add] command, but I do not know how!");
+
     }
 
 }
