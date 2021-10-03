2f3af04fc1266f9843afaaabf2587228b2d58f2b
theeugenechong <eugenechong20011@yahoo.com>
2021-10-03 15:29:33 +0800

    Change Cooper's output style Make Ui class have all static methods

--- src/main/java/cooper/ui/Ui.java
+++ src/main/java/cooper/ui/Ui.java
@@ -1,115 +1,105 @@
 package cooper.ui;
 
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Ui {
 
-    static final String LOGO =
-        "              ,----..       ,----..   ,-.----.                   \n"
-        + "            /   /   \\     /   /   \\  \\    /  \\                   \n" 
-        + "           /   .     :   /   .     : |   :    \\                  \n" 
-        + "          .   /   ;.  \\ .   /   ;.  \\|   |  .\\ :          __  ,-.\n" 
-        + "         .   ;   /  ` ;.   ;   /  ` ;.   :  |: |        ,' ,'/ /|\n" 
-        + "   ,---. ;   |  ; \\ ; |;   |  ; \\ ; ||   |   \\ : ,---.  '  | |' |\n" 
-        + "  /     \\|   :  | ; | '|   :  | ; | '|   : .   //     \\ |  |   ,'\n" 
-        + " /    / '.   |  ' ' ' :.   |  ' ' ' :;   | |`-'/    /  |'  :  /  \n" 
-        + " .    ' / '   ;  \\; /  |'   ;  \\; /  ||   | ;  .    ' / ||  | '   \n" 
-        + " '   ; :__ \\   \\  ',  /  \\   \\  ',  / :   ' |  '   ;   /|;  : |   \n" 
-        + " '   | '.'| ;   :    /    ;   :    /  :   : :  '   |  / ||  , ;   \n" 
-        + " |   :    :  \\   \\ .'      \\   \\ .'   |   | :  |   :    | ---'    \n" 
-        + "  \\   \\  /    `---`         `---`     `---'.|  \\  \\  /          \n" 
-        + "  `----'                               `---`    `----'            \n";
+    private static final String LOGO = "            /$$$$$$   /$$$$$$  /$$$$$$$                    \n"
+            +                  "           /$$__  $$ /$$__  $$| $$__  $$                   \n"
+            +                  "  /$$$$$$$| $$  \\ $$| $$  \\ $$| $$  \\ $$ /$$$$$$   /$$$$$$ \n"
+            +                  " /$$_____/| $$  | $$| $$  | $$| $$$$$$$//$$__  $$ /$$__  $$\n"
+            +                  "| $$      | $$  | $$| $$  | $$| $$____/| $$$$$$$$| $$  \\__/\n"
+            +                  "| $$      | $$  | $$| $$  | $$| $$     | $$_____/| $$      \n"
+            +                  "|  $$$$$$$|  $$$$$$/|  $$$$$$/| $$     |  $$$$$$$| $$      \n"
+            +                  " \\_______/ \\______/  \\______/ |__/      \\_______/|__/      ";
 
-    static final String GREETING =
-        "____________________________________________________________\n"
-        + "Hello! I'm Cooper\n"
-        + "What can I do for you?\n";
+    private static final String LINE = "=========================================================================";
 
-    static final String SEPERATOR = "---------------";
+    private static final String GREETING = "Hello I'm cOOPer! Nice to meet you!";
 
-    private PrintStream printStream;
-    private Scanner scanner;
-
-    /**
-     * specify the input and output streams that ui interface with.
-     */
-    public Ui(InputStream inputStream, PrintStream printStream) {
-        this.printStream = printStream;
-        this.scanner = new Scanner(System.in);
-    }
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final PrintStream printStream = System.out;
 
 
-    public String getInput() {
+    public static String getInput() {
+        showPrompt();
         return scanner.nextLine();
     }
 
-    public void showLogo() {
+    public static void showLogo() {
         show(LOGO);
     }
 
-    public void showGreetingMessage() {
+    public static void showIntroduction() {
+        show(LINE);
         show(GREETING);
+        show("Login or register to gain access to my features!");
+        show("To login, enter [login /u yourUsername /as yourRole]");
+        show("To register, enter [register /u yourUsername /as yourRole]");
+        show(LINE);
     }
 
-    public void showSeperator() {
-        show(SEPERATOR);
-    }
-
-    public void showText(String text) {
+    public static void showText(String text) {
         show(text);
     }
 
     /**
      * Exception message to show file path error.
      **/
-    public void showInvalidFilePathError() {
+    public static void showInvalidFilePathError() {
+        show(LINE);
         show("Parser received invalid input file path!");
-        showSeperator();
+        show(LINE);
     }
 
     /**
      * Exception message to show invalid command error.
      **/
-    public void showInvalidCommandError() {
-        show("I don't quite understand what do you mean, try type in [help] to see available commands.");
-        showSeperator();
+    public static void showUnrecognisedCommandError(String input) {
+        show(LINE);
+        showText("Unrecognised command: " + input);
+        show("I don't recognise the command you entered. Enter [help] to view available commands.");
+        show(LINE);
     }
 
     /**
      * Exception message to show invalid command argument error.
      **/
-    public void showInvalidCommandArgumentError() {
-        show("You have keyed in invalid command argument.");
-        showSeperator();
+    public static void showInvalidCommandArgumentError() {
+        show(LINE);
+        show("You have keyed in invalid command arguments.");
+        show(LINE);
     }
 
     /**
      * Exception message to show invalid number (such as task id) error.
      **/
-    public void showInvalidNumberError() {
+    public static void showInvalidNumberError() {
+        show(LINE);
         show("Unrecognised number.");
-        showSeperator();
+        show(LINE);
     }
 
-    public void showBye() {
-        show("Bye bye :))");
+    public static void showBye() {
+        show(LINE);
+        show("Bye, see you next time! :D");
+        show(LINE);
     }
 
-    public void showPrompt() {
+    public static void showPrompt() {
         printStream.print(">> ");
     }
 
     /**
      * Close streams properly.
      */
-    public void closeStreams() {
+    public static void closeStreams() {
         scanner.close();
         printStream.close();
     }
 
-    private void show(String printMessage) {
+    private static void show(String printMessage) {
         printStream.println(printMessage);
     }
 
