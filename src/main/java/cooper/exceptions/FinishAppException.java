0deb14c894d1e9019589d11f74743170435dc54a
theeugenechong <eugenechong20011@yahoo.com>
2021-10-03 15:28:42 +0800

    Add and remove exception classes

--- src/main/java/cooper/exceptions/FinishAppException.java
+++
@@ -1,5 +0,0 @@
-package cooper.exceptions;
-
-public class FinishAppException extends Exception {
-  //no other code needed
-}
---
+++ src/main/java/cooper/exceptions/InvalidCommandFormatException.java
@@ -0,0 +1,5 @@
+package cooper.exceptions;
+
+public class UnrecognisedCommandException extends Exception {
+  //no other code needed
+}
