1e644480dc7070d1e949cc6909ae173ff08abdca
shixi <fanshixi123@gmail.com>
2021-10-04 23:20:08 +0800

    Fix meetings hashmap to take in values as an arraylist type instead of string

--- src/main/java/cooper/meetings/MeetingManager.java
+++ src/main/java/cooper/meetings/MeetingManager.java
@@ -2,20 +2,23 @@ package cooper.meetings;
 
 import cooper.ui.Ui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MeetingManager {
-    private static HashMap<String, String> meetings = new HashMap<>(10);
+    private static HashMap<String, ArrayList<String>> meetings = new HashMap<>(10);
 
     public static void addAvailability(String time, String name) {
-        meetings.put(time, name);
+        if (!meetings.containsKey(time)) {
+            meetings.put(time, new ArrayList<>());
+        }
+        meetings.get(time).add(name);
     }
 
     public static void printAvailabilities() {
         Ui.printMeetingTableHeader();
         for (String key: meetings.keySet()) {
             Ui.showText(key + " | " + meetings.get(key));
-            //System.out.println(key + " \\| " + meetings.get(key));
         }
     }
 }
