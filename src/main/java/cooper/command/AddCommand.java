a19d02e1b76febb157856e2b2fb57fc1f6042323
ChrisLangton <christwlangton@gmail.com>
2021-10-08 02:38:20 +0800

    add documentation, some minor bug fixes and some enhancement of balance sheet

--- src/main/java/cooper/command/AddCommand.java
+++ src/main/java/cooper/command/AddCommand.java
@@ -5,6 +5,9 @@ import cooper.ui.Ui;
 import cooper.finance.FinanceManager;
 import cooper.verification.SignInDetails;
 
+/**
+ * The child class of Command that handles the 'add' command specifically
+ */
 public class AddCommand extends Command {
 
     public boolean isInflow;
@@ -17,8 +20,12 @@ public class AddCommand extends Command {
     }
 
     @Override
+    /**
+     * The override function for executing the 'add' command, calls for 'add' and subsequently printing the status
+     * to the command line.
+     */
     public void execute(SignInDetails signInDetails, FinanceManager financeManager, MeetingManager meetingManager) {
-        financeManager.addBalance(Integer.parseInt(amount));
+        financeManager.addBalance(Integer.parseInt(amount), isInflow);
         Ui.printAddCommand(amount, isInflow);
     }
 }
--- src/main/java/cooper/finance/FinanceManager.java
+++ src/main/java/cooper/finance/FinanceManager.java
@@ -2,6 +2,9 @@ package cooper.finance;
 
 import java.util.ArrayList;
 
+/**
+ * Handles all actions and operations pertaining to financial assistance functions of the application
+ */
 public class FinanceManager {
     private final ArrayList<Integer> balanceSheet;
 
@@ -9,12 +12,26 @@ public class FinanceManager {
         balanceSheet = new ArrayList<>();
     }
 
+    /**
+     * Getter for the Balance Sheet
+     * @return balanceSheet ArrayList
+     */
     public ArrayList<Integer> getBalanceSheet() {
         return balanceSheet;
     }
 
-    public void addBalance(int amount) {
-        balanceSheet.add(amount);
+    /**
+     * Adds specified amount input by user to the balanceSheet, with specified inflow or outflow
+     * @param amount
+     * @param isInflow
+     */
+    public void addBalance(int amount, boolean isInflow) {
+        if (isInflow) {
+            balanceSheet.add(amount);
+        }
+        else {
+            balanceSheet.add(amount * -1);
+        }
     }
 
 }
