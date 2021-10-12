ea4a360432b7cf4682f22ed321ffad1510bad6a0
theeugenechong <eugenechong20011@yahoo.com>
2021-10-12 10:13:06 +0800

    Add JavaDoc for classes in verification package

--- src/main/java/cooper/verification/SignInProtocol.java
+++ src/main/java/cooper/verification/SignInProtocol.java
@@ -2,8 +2,13 @@ package cooper.verification;
 
 import java.util.HashMap;
 
+/**
+ * Class representing protocols which allow a user to gain access to cOOPer's features, namely
+ * login and registration.
+ */
 public abstract class SignInProtocol {
 
+    /* Represents the sign-in details related to this sign in protocol instance. */
     protected final SignInDetails signInDetails;
 
     public SignInProtocol(SignInDetails signInDetails) {
@@ -12,6 +17,14 @@ public abstract class SignInProtocol {
 
     public abstract void executeSignIn(Verifier verifier, HashMap<String, UserRole> registeredUsers);
 
+    /**
+     * Checks if the username in {@code signInDetails} is already present in the list of registered
+     * users.
+     *
+     * @param registeredUsers A list of users already registered with cOOPer along with their respective
+     *                        roles.
+     * @return true if the username of {@code signInDetails} is already present as a key in {@code registeredUsers}
+     */
     public boolean isRegisteredUser(HashMap<String, UserRole> registeredUsers) {
         return registeredUsers.containsKey(signInDetails.getUsername());
     }
