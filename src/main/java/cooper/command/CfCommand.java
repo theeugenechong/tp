48fec177570b84ea68204a2721ab6a2108cf0baf
theeugenechong <eugenechong20011@yahoo.com>
2021-10-26 00:34:57 +0800

    Provide functionality to reset balance sheet/cf statement whenever user re-enters the bf/cs command

--- src/main/java/cooper/command/CfCommand.java
+++ src/main/java/cooper/command/CfCommand.java
@@ -1,12 +1,15 @@
 package cooper.command;
 
 import cooper.exceptions.InvalidAccessException;
+import cooper.finance.CashFlow;
 import cooper.finance.FinanceCommand;
+import cooper.finance.FinanceManager;
 import cooper.parser.CommandParser;
 import cooper.resources.ResourcesManager;
 import cooper.storage.StorageManager;
 import cooper.ui.Ui;
 import cooper.verification.SignInDetails;
+import cooper.verification.UserRole;
 
 public class CfCommand extends Command {
 
@@ -18,6 +21,20 @@ public class CfCommand extends Command {
     public void execute(SignInDetails signInDetails, ResourcesManager resourcesManager,
                         StorageManager storageManager) throws InvalidAccessException {
         CommandParser.financeFlag = FinanceCommand.CF;
+        UserRole userRole = signInDetails.getUserRole();
+        FinanceManager financeManager = resourcesManager.getFinanceManager(userRole);
+        if (financeManager == null) {
+            Ui.printAdminHelp();
+            Ui.printGeneralHelp();
+            throw new InvalidAccessException();
+        }
+        resetCashFlowStatement(financeManager);
         Ui.initiateCashFlowStatement();
+    }
+
+    private void resetCashFlowStatement(FinanceManager financeManager) {
+        CashFlow cashFlow = financeManager.cooperCashFlowStatement;
+        CashFlow.cashFlowStage = 0;
+        cashFlow.getCashFlowStatement().clear();
     }
 }
