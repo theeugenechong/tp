79c054839129ea6d730181047f19d3168e8d1baf
theeugenechong <eugenechong20011@yahoo.com>
2021-10-27 03:05:40 +0800

    Refactor some code

--- src/main/java/cooper/storage/ForumStorage.java
+++ src/main/java/cooper/storage/ForumStorage.java
@@ -1,10 +1,9 @@
 package cooper.storage;
 
-import cooper.exceptions.InvalidFileDataException;
 import cooper.exceptions.InvalidForumPostIdException;
-import cooper.finance.CashFlow;
-import cooper.finance.FinanceManager;
+import cooper.forum.ForumComment;
 import cooper.forum.ForumManager;
+import cooper.forum.ForumPost;
 import cooper.ui.FileIoUi;
 
 import java.io.FileWriter;
@@ -22,7 +21,7 @@ public class ForumStorage extends Storage {
         int currentPost = 0;
         Scanner fileScanner = getScanner(filePath);
         while (fileScanner.hasNext()) {
-            String[] post = fileScanner.nextLine().split("[|]");
+            String[] post = fileScanner.nextLine().split("\\|");
             if (post[0].equals("P")) {
                 currentPost++;
                 forumManager.addPost(post[1],post[2]);
@@ -38,24 +37,21 @@ public class ForumStorage extends Storage {
         }
     }
 
-
     public void saveForum(ForumManager forumManager) { 
         try {
             FileWriter fileWriter = new FileWriter(filePath, false);
-            var forumPosts = forumManager.getForumPosts();
-            for (var post : forumPosts) {
+            ArrayList<ForumPost> forumPosts = forumManager.getForumPosts();
+            for (ForumPost post : forumPosts) {
+
                 fileWriter.write("P|" + post.getUsername() + "|" + post.getContent() + "\n");
-                for (var comment : post.getComments()) {
+                for (ForumComment comment : post.getComments()) {
                     fileWriter.write("C|" + comment.getUsername() + "|" + comment.getContent() + "\n");
                 }
             }
-
             fileWriter.close();
         } catch (IOException e) {
             FileIoUi.showFileWriteError(e);
             System.exit(1);
         }
-
     }
-
 }
